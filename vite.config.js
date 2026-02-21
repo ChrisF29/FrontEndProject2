4531fb6886f16d75b49a43fbf3eee78c93e79f0e
Chris Ferrer <imjunjun24@gmail.com>
2026-02-21 22:01:21 +0800

    Refactor code structure for improved readability and maintainability

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/FrontEndProject2/",
   plugins: [react(), tailwindcss()],
 });
